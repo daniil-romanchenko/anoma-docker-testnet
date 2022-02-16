1298ac6827f0bbdea5c6ddefabc69c5f0dc38367
6696 <daniil.romanchenko@gmail.com>
2022-02-16 00:30:19 +0000

    cascade network statup

--- compose/start_new_network.sh
+++ compose/start_new_network.sh
@@ -8,18 +8,24 @@ pids=()
 ./tasks compose up --build > logs/testnet.log &
 pids+=( $! )
 
-sleep 8
+sleep 10
+echo 'Starting ledger...'
 anoma ledger > logs/ledger.log &
 pids+=( $! )
 
 
 sleep 5
-anoma node gossip --rpc "127.0.0.1:26660" > logs/gossip.log 
-#pids+=( $! )
+echo 'Starting gossip node...'
+anoma node gossip --rpc "127.0.0.1:26660" > logs/gossip.log &
+pids+=( $! )
 
 #sleep 2
 #anoma node matchmaker --matchmaker-path libmm_token_exch.so --tx-code-path tx_from_intent.wasm --ledger-address "127.0.0.1:26657" --source my-matchmaker --signing-key my-matchmaker > mmaker.log 
 
+echo 'Running!'
+( trap exit SIGINT ; read -r -d '' _ </dev/tty )
+
+echo 'Killing taks...'
 for pid in "${pids[@]}"; do
         kill $pid
 done
