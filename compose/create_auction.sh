d3203cfe7165c276ec1f17f16c1cbc6534d27642
daniil <daniil.romanchenko@gmail.com>
2022-05-01 17:29:41 +0200

    Add bob, increase clearance time, change amount of potatoes to be put on auction

--- compose/create_auction.sh
+++ compose/create_auction.sh
@@ -9,12 +9,28 @@ anomac init-account --source alberto-key --public-key alberto-key --alias albert
 
 anoma client transfer --source faucet --target alberto-account --signer alberto-key --token btc --amount 1
 anoma client transfer --source faucet --target alberto-account --signer alberto-key --token kartoffel --amount 142
+anoma client update --address alberto-account --code-path /home/daniil/IdeaProjects/anoma5/anoma-docker-testnet/compose/permissive-vp.wasm
 
 echo "Alberto BTC balance:"
 anomac balance --token btc --owner alberto-account
 
 echo "Alberto Kartoffel balance:"
 anomac balance --token kartoffel --owner alberto-account
+
+
+anomaw address gen --alias bob-key --unsafe-dont-encrypt
+anomac init-account --source bob-key --public-key bob-key --alias bob-account
+
+anoma client transfer --source faucet --target bob-account --signer bob-key --token btc --amount 17
+anoma client transfer --source faucet --target bob-account --signer bob-key --token kartoffel --amount 1
+anoma client update --address bob-account --code-path /home/daniil/IdeaProjects/anoma5/anoma-docker-testnet/compose/permissive-vp.wasm
+
+echo "Bob BTC balance:"
+anomac balance --token btc --owner bob-account
+
+echo "Alberto Kartoffel balance:"
+anomac balance --token kartoffel --owner bob-account
+
 
 export ALBERTO=$(anoma wallet address find --alias alberto-account | cut -c 28- | tr -d '\n')
 export BTC=$(anoma wallet address find --alias btc | cut -c 28- | tr -d '\n')
@@ -24,7 +40,7 @@ NOW=$(date +%s)
 
 START=$(($NOW + 30))
 END=$(($NOW + 70))
-CLEARANCE=$(($NOW + 270))
+CLEARANCE=$(($NOW + 260))
 
 echo 'Start: '
 echo $(date -d @$START)
@@ -36,7 +52,7 @@ echo 'Clearance: '
 echo $(date -d @$CLEARANCE)
 
 
-echo '[{"addr":"'$ALBERTO'","create_auction":{"token_sell": "'$KTF'","token_buy": "'$BTC'","amount": "7","auction_start": "'$START'","auction_end": "'$END'", "auction_clearance": "'$CLEARANCE'"}}]' > auction.json
+echo '[{"addr":"'$ALBERTO'","create_auction":{"token_sell": "'$KTF'","token_buy": "'$BTC'","amount": "10","auction_start": "'$START'","auction_end": "'$END'", "auction_clearance": "'$CLEARANCE'"}}]' > auction.json
 
 
 anoma auction-intent --node "http://127.0.0.1:26660" --signing-key alberto-key --data-path auction.json --topic "asset_v1"
