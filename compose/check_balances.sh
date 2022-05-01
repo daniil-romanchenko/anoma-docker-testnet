654b6d988bc403832ff96254e27c34d26309ea78
daniil <daniil.romanchenko@gmail.com>
2022-05-01 17:25:39 +0200

    Small script to check balances

---
+++ compose/check_balances.sh
@@ -0,0 +1,12 @@
+echo "Alberto BTC balance:"
+anomac balance --token btc --owner alberto-account
+
+echo "Alberto Kartoffel balance:"
+anomac balance --token kartoffel --owner alberto-account
+
+echo "Bob BTC balance:"
+anomac balance --token btc --owner bob-account
+
+echo "Bob Kartoffel balance:"
+anomac balance --token kartoffel --owner bob-account
+
