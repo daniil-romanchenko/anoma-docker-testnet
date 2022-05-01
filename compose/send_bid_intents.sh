deb8262c15a25d5f55081c072a797140d6b4c4a3
daniil <daniil.romanchenko@gmail.com>
2022-05-01 17:30:02 +0200

    Send 4th Intent as bob

--- compose/send_bid_intents.sh
+++ compose/send_bid_intents.sh
@@ -2,7 +2,7 @@
 anoma auction-intent --node "http://127.0.0.1:26660" --signing-key alberto-key --data-path bid1.json --topic "asset_v1"
 anoma auction-intent --node "http://127.0.0.1:26660" --signing-key alberto-key --data-path bid2.json --topic "asset_v1"
 anoma auction-intent --node "http://127.0.0.1:26660" --signing-key alberto-key --data-path bid3.json --topic "asset_v1"
-anoma auction-intent --node "http://127.0.0.1:26660" --signing-key alberto-key --data-path bid4.json --topic "asset_v1"
+anoma auction-intent --node "http://127.0.0.1:26660" --signing-key bob-key --data-path bid4.json --topic "asset_v1"
 
 ########################
 
